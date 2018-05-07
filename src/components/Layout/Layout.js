82e80c9572e76932be6a95886ad60828e7183381
linh626688@gmail.com <cavoixanh>
2018-05-08 00:03:08 +0700

    113 update project with sample

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,13 @@ import React from 'react';
 import Auo from '../../hoc/Auo';
 import classes from './Layout.css';
 
-const layout = (props) => (
+const layout = ( props ) => (
     <Auo>
-        <div> Toolbar , SlideDrawer, BackDrop</div>
+        <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
         </main>
     </Auo>
 );
+
 export default layout;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,18 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
 import Auo from '../../hoc/Auo';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    render()
-    {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
+    render () {
         return (
             <Auo>
-                <Burger/>
-                <div>
-                    Build Controls
-                </div>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
             </Auo>
-        )
+        );
     }
 }
+
 export default BurgerBuilder;
